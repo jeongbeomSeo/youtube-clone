a8e9f37802e8bc00e85ae0972f7a7fc45d93cc5c
jeongbeomSeo <jeongbeom4693@gmail.com>
2022-01-04 19:14:26 +0900

    youtube clone initial server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+app.get("/")
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
+
+app.listen(PORT, handleListening);
