a97c87df817d5b3f45a4dd8d8789e0c9e462df3b
jeongbeomSeo <jeongbeom4693@gmail.com>
2022-01-05 16:47:26 +0900

    Add express server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.get("/")
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
