dd9bbf77e89c7cb504350678ad53cba72391c3d1
jeongbeomSeo <jeongbeom4693@gmail.com>
2022-05-12 20:26:25 +0900

    isHeroku Error correcting

--- src/middlewares.js
+++ src/middlewares.js
@@ -27,7 +27,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
-  res.locals.isHeorku = isHeorku;
+  res.locals.isHeroku = isHeorku;
   next();
 };
 
